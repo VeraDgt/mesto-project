c48c8d0d97b0717b65f7ae4414ff91c031b10ef2
VeraDgt <116259787+VeraDgt@users.noreply.github.com>
2023-07-13 14:55:56 +0300

    Revert "Update index.js"

--- src/pages/index.js
+++ src/pages/index.js
@@ -157,7 +157,7 @@ popupEditAvatar.setEventListeners();
 
 avatarEditButton.addEventListener('click', () => {
   popupEditAvatar.open();
-  changeAvatarValidation.resetValidation()
+  changeAvatarValidation.resetValidation(popupSubmit)
 });
 
 
@@ -186,7 +186,7 @@ popupAddCard.setEventListeners();
 
 cardAddButton.addEventListener('click', () => {
   popupAddCard.open();
-  addFormValidaton.resetValidation()
+  addFormValidaton.resetValidation(popupSubmit)
 });
 
 const popupConfirmDelete = new PopupWithConfirmation({
